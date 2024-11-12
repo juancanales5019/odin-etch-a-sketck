8e9535b942bdfda1638f8e0b438c2f481e4177ec
Juan Pineda <juancanales5019@gmail.com>
2024-11-11 20:19:33 -0600

    create a function to create a initial grid onload
    I had to research how to do this and I found
    window.onload...
    
    Also move the function out of the event listener
    of the button.

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 let counter = document.querySelector(".counter");
 let container = document.querySelector(".container");
 
-counter.addEventListener("click",()=> {
+window.onload = function() {
+    // Your DOM manipulation code here
+    container.textContent = "";
+    nSquares = 16;
+    for ( let n = 0 ; n < nSquares; n++){
+        let row = document.createElement("div");
+        row.style = "background: blue";
+        row.style = "display: flex";
+        row.style.flexGrow = "1"
+        for(let i = 0; i< nSquares; i++){
+            let element = document.createElement("div");
+            element.style = "background: blue";
+            element.style = "flex: 1 1 0";
+            row.appendChild(element);
+            element.addEventListener("mouseenter",()=>{
+                element.style.backgroundColor = "red";
+            });
+            element.addEventListener("mouseleave",()=>{
+                element.style.backgroundColor = "blue";
+            });
+        }
+        container.appendChild(row);
+    }
+};
+
+let squareDeployer = ()=>{
     container.textContent = "";
     nSquares = parseInt(prompt("Enter the Number of element per column and row"));
     for ( let n = 0 ; n < nSquares; n++){
@@ -23,4 +48,8 @@ counter.addEventListener("click",()=> {
         }
         container.appendChild(row);
     }
+};
+counter.addEventListener("click",()=> {
+    squareDeployer();
 });
+
