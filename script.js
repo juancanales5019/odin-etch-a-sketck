3b4006d1ce06ef7a0c18f8718f6174567a477f16
Juan Pineda <juancanales5019@gmail.com>
2024-11-11 21:04:55 -0600

    Arrange function and delete duplicated committed
    found a way to write the generate x function once

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let counter = document.querySelector(".counter");
 let container = document.querySelector(".container");
-
-window.onload = function() {
-    // Your DOM manipulation code here
+// function to display x amount of div squares
+let displayXSquares = (nSquares)=>{
     container.textContent = "";
-    nSquares = 16;
     for ( let n = 0 ; n < nSquares; n++){
         let row = document.createElement("div");
         row.style = "background: blue";
         row.style = "display: flex";
         row.style.flexGrow = "1"
+        row.style.border = "0";
+        row.style.borderRadius = "0"
         for(let i = 0; i< nSquares; i++){
             let element = document.createElement("div");
             element.style = "background: blue";
             element.style = "flex: 1 1 0";
+            element.style.backgroundColor = "skyblue"
+            element.style.borderRadius = "0";
             row.appendChild(element);
             element.addEventListener("mouseenter",()=>{
                 element.style.backgroundColor = getRandomColor();
@@ -24,6 +26,11 @@ window.onload = function() {
         }
         container.appendChild(row);
     }
+};
+// function to deploy 16 by 16 div squares onload
+window.onload = function() {
+    // Your DOM manipulation code here
+    displayXSquares(16);
 };
 // Function to generate a random color
 function getRandomColor() {
@@ -34,29 +41,13 @@ function getRandomColor() {
     }
     return color;
 }
+// function to generate user x by x squares on container
 let squareDeployer = ()=>{
     container.textContent = "";
-    nSquares = parseInt(prompt("Enter the Number of element per column and row"));
-    for ( let n = 0 ; n < nSquares; n++){
-        let row = document.createElement("div");
-        row.style = "background: blue";
-        row.style = "display: flex";
-        row.style.flexGrow = "1"
-        for(let i = 0; i< nSquares; i++){
-            let element = document.createElement("div");
-            element.style = "background: blue";
-            element.style = "flex: 1 1 0";
-            row.appendChild(element);
-            element.addEventListener("mouseenter",()=>{
-                element.style.backgroundColor = getRandomColor();
-            });
-            element.addEventListener("mouseleave",()=>{
-                element.style.backgroundColor = getRandomColor();
-            });
-        }
-        container.appendChild(row);
-    }
+    userInput = parseInt(prompt("Enter the Number of element per column and row"));
+    displayXSquares(userInput);
 };
+// Call the squareDeployer when user click and choose a number
 counter.addEventListener("click",()=> {
     squareDeployer();
 });
